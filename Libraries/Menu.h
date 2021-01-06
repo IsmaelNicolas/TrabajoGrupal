3bcc9d8592637239446be068d41571f85e203c2b
Matitisxd <rodriguezmatitis@gmail.com>
2021-01-06 02:14:42 -0500

    Menu

--- Libraries/Menu.h
+++ Libraries/Menu.h
@@ -1,3 +1,9 @@
+/***********************************************************************
+ * Module:  Menu.h
+ * Author:  USUARIO
+ * Modified: jueves, 28 de mayo de 2020 19:07:11
+ * Purpose: Declaration of the class Menu
+ ***********************************************************************/
 
 #if !defined(__Menu)
 #define __Menu
@@ -8,7 +14,18 @@
 using namespace std;
 
 class Menu {
-
+public:
+    template<typename T>
+    int options(T* option, int index);
+    template<typename T>
+    int options(T* option, int index, int color);
+    template<typename T>
+    int options(char* message ,T* option, int index);
+    template<typename T>
+    int options(char* message, T* option, int index, int color);
+private:
+    Screen screen;
+protected:
 };
 
 #endif
--- Libraries/Screen.h
+++ Libraries/Screen.h
@@ -1,4 +1,9 @@
-
+/***********************************************************************
+ * Module:  Screen.h
+ * Author:  USUARIO
+ * Modified: jueves, 28 de mayo de 2020 19:07:11
+ * Purpose: Declaration of the class Screen
+ ***********************************************************************/
 
 #if !defined(__Screen)
 #define __Screen
