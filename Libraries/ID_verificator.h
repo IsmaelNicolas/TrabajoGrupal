bf8165061111643e0ced14c375f33f84f2cec6bb
Dennis Parra <dennis_parra1999@hotmail.com>
2021-01-06 02:41:40 -0500

    Commit

--- Libraries/ID_verificator.h
+++ Libraries/ID_verificator.h
@@ -1,1 +1,15 @@
+#if !defined(__ID_verificator_h)
+#define __ID_verificator_h
 
+using namespace std;
+
+class ID_verificator{
+public:
+	bool ID_verify(char* ID);
+protected:
+	bool is_between(int min, int number, int max);
+	int next_ten(int number);
+	bool check_last_digit(char* ID);
+};
+
+#endif
