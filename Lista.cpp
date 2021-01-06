7d3b343ef1adc79e88998985501f077d73b7ec45
camilavenegas <cami_vene@hotmail.com>
2021-01-06 02:12:00 -0500

    metodo insertar al inicio

--- Lista.cpp
+++ Lista.cpp
@@ -1,1 +1,51 @@
 
+#include "Lista.h"
+#include <iostream>
+
+using namespace std;
+bool Lista::listaVacia()
+{
+    return lista==NULL;
+}
+
+Lista::Lista()
+{
+    this->lista = NULL;
+    this->listaFinal = NULL;
+}
+
+int Lista::tamano()
+{
+    Nodo* aux = new Nodo();
+    Nodo* aux1 = new Nodo();
+
+    aux = this->lista;
+
+    int posicion = 0;
+    while (aux != this->listaFinal)
+    {
+        posicion++;
+        aux1 = aux;
+        aux = aux->getSiguiente();
+    }
+
+    return posicion + 1;
+}
+
+void Lista::insertarInicio(Persona dato)
+{
+    if (this->listaVacia())
+    {
+        this->lista = new Nodo(dato, this->listaFinal, this->listaFinal);
+        this->listaFinal = this->lista;
+        cout <<"EXITO";
+    }
+    else
+    {
+        Nodo* aux = new Nodo(dato, lista, this->listaFinal);
+        this->lista->setAnterior(aux);
+        this->listaFinal->setSiguiente(aux);
+        this->lista = aux;
+    }
+
+}
