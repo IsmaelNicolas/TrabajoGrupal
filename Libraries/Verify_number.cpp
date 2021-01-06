79497ab43f08c563f7e5430bcb63112e85cd2aff
Angel Castillo <51182768+Agcastillo2@users.noreply.github.com>
2021-01-06 02:45:00 -0500

    validar numeros,letras

--- Libraries/Verify_number.cpp
+++ Libraries/Verify_number.cpp
@@ -1,1 +1,42 @@
+#define _GLIBCXX_USE_CXX11_ABI 0
+
+#include "Verify_number.h"
+
+#include <iostream>
+#include <string>
+#include <ctype.h>
+
+#pragma once
+
+using namespace std;
+
+/**
+ * @brief is_int_number
+ * @param string_1
+ * @return bool
+*/
+bool is_int_number(string string_1) {
+	for (char aux: string_1) {
+		if (!isdigit(aux)) {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
+
+/**
+ * @brief is_float_number
+ * @param string_1
+ * @return bool
+*/
+bool is_float_number(string string_1) {
+	for (char aux : string_1) {
+		if (!isdigit(aux) && aux != '.') {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
 
--- Libraries/Verify_number.h
+++ Libraries/Verify_number.h
@@ -1,1 +1,9 @@
+#if !defined(__Verify_number_h)
+#define __Verify_number_h
 
+using namespace std;
+
+bool is_int_number(string string_1);
+bool is_float_number(string string_1);
+
+#endif
